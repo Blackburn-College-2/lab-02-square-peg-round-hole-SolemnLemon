56368d4561c884bbd4acef4617c8e7ea4b17e07a
SolemnLemon <sunbear.bonzai@gmail.com>
2020-01-30 10:52:22 -0600

    first commit got up through step 5 done nothing on step 6 yet steps 6-9 to do

---
+++ src/emptyjavaproject/Measurement.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emptyjavaproject;
+
+/**
+ *
+ * @author sunbe
+ */
+public class Measurement {
+
+    double value = 0.0;
+    String units = "";
+    String valueWithUnits;
+
+    public Measurement(double value, String units) {
+        this.value = value;
+        this.units = units;
+        valueWithUnits=toString(value,units);
+    }
+
+    public String toString(double value, String units) {
+        String s = String.valueOf(value) + " " + units;
+
+        return s;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+}
--- src/emptyjavaproject/Square.java
+++ src/emptyjavaproject/Square.java
@@ -7,15 +7,12 @@ package emptyjavaproject;
 
 /**
  *
- * @author paul.kline
+ * @author sunbe
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Square extends Rectangle{
+    Measurement sideLength;
+    public Square(Measurement l, Measurement w){
+        super(l,w);
+    this.sideLength=l;
     }
-    
 }
